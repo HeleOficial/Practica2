42dd5edc2f9c346270e2649b3843b1d5ccf2fe25
HeleOficial <juanledesmaidarraga@gmail.com>
2025-04-11 19:56:04 -0500

    commit inicial se crea Clase Criatura

---
+++ Practica2/src/practica2/Magico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package practica2;
+
+/**
+ *
+ * @author EIMY TATIANA CORAL M
+ */
+public interface Magico {
+    
+    void LanzarHechizo();
+    void AprenderHechizo();
+    
+}
